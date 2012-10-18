11d699d9e8bfc860ddf47cecf2e7496f2e92fcaf
Ray Davis <ray@media.berkeley.edu>
2012-10-18 11:43:54 -0700

    rails-api new ray_api --old-style-hash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RayApi::Application.config.secret_token = '478da50ba621b9746106376275d42923b1d3eafbf106620c89ca3e352b9eb662bc2fc38e419afd4362a5cd57e88988f372f2966d5e5c31de5289d2966dc9cf9f'
