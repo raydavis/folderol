cb44a5843040fddf26f8dd5c8b487fb80a97ee62
Ray Davis <ray@media.berkeley.edu>
2012-10-23 10:08:15 -0700

    Merge campus data with local data in UX

--- cc_app/api.py
+++ cc_app/api.py
@@ -1,8 +1,10 @@
+from tastypie import fields, utils
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from cc_app.models import CalCentralUser
 
 class UserResource(ModelResource):
+    preferred_name = fields.CharField(attribute="preferred_name", readonly=True)
     class Meta:
         queryset = CalCentralUser.objects.all()
         resource_name = 'user'
--- cc_app/models.py
+++ cc_app/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.db.models.signals import post_init
+from django.dispatch import receiver
+from campus_models import CampusPersonAttributes
 
 class CalCentralUser(models.Model):
     uid = models.CharField(max_length=40)
-    preferred_name = models.CharField(max_length=400)
+    custom_preferred_name = models.CharField(max_length=400, db_column='preferred_name')
+    campus_attributes = None
+
     def __unicode__(self):
         return str(vars(self))
+
+    @property
+    def preferred_name(self):
+        print 'custom_preferred_name=', self.custom_preferred_name
+        if not self.campus_attributes:
+            self.campus_attributes = CampusPersonAttributes.objects.get(pk=self.uid)
+        print 'campus_attributes=', self.campus_attributes
+        val = self.custom_preferred_name or self.campus_attributes.person_name
+        return val
+    @preferred_name.setter
+    def preferred_name(self, value):
+        self.custom_preferred_name = value
